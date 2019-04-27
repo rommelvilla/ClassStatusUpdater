43f103ace809b87457d825a3d043d8b0070025d2
Simon Timms <stimms@gmail.com>
2019-04-26 22:01:18 -0600

    Setting task name

---
+++ ClassStatusUploader/TaskController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ClassStatusUploader.Hubs;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ClassStatusUploader
+{
+    [Route("api/[controller]")]
+    public class TaskController : Controller
+    {
+        readonly IHubContext<TeamHub> hub;
+        public TaskController(IHubContext<TeamHub> hub)
+        {
+            this.hub = hub;
+        }
+
+        [HttpPost]
+        public async Task Post([FromBody]string value)
+        {
+            await hub.Clients.All.SendAsync("TaskUpdated", value);
+        }
+
+    }
+}
