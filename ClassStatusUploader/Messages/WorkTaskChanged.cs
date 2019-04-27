c3a2249c158041f1835b2c54db7596592ed30ceb
Sean Feldman <feldman.sean@gmail.com>
2019-04-27 10:27:28 -0600

    Minor cleanup (fixing my OCD)

--- ClassStatusUploader/Messages/WorkTaskChanged.cs
+++ ClassStatusUploader/Messages/WorkTaskChanged.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Linq;
-
-namespace ClassStatusUploader.Messages
+﻿namespace ClassStatusUploader.Messages
 {
     public class WorkTaskChanged
     {
--- ClassStatusUploader/Startup.cs
+++ ClassStatusUploader/Startup.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using ClassStatusUploader.Hubs;
+﻿using ClassStatusUploader.Hubs;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -18,7 +13,8 @@ namespace ClassStatusUploader
             Configuration = configuration;
         }
 
-        public IConfiguration Configuration { get; }
+        IConfiguration Configuration { get; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -44,13 +40,15 @@ namespace ClassStatusUploader
             {
                 app.UseDeveloperExceptionPage();
             }
+
             app.UseCors("CorsPolicy");
+
             app.UseSignalR(routes =>
             {
                 routes.MapHub<TeamHub>("/teamHub");
             });
+
             app.UseMvcWithDefaultRoute();
-            
         }
     }
 }
